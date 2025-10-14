61abdc082ddd89e1c4251e522fa749180ccbdd62
Nazanin Moosazade <moosazadenazanin@gmail.com>
2025-10-14 17:57:49 +0330

    create pastries page

---
+++ src/app/pastries/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '@/components/modules/navbar/Navbar'
+
+const page = () => {
+  return (
+    <>
+    <Navbar/>
+    <h1>pastries</h1>
+    </>
+  )
+}
+
+export default page
